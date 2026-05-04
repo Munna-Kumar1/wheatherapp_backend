67779b85f4ccefb61375ff528ac0d9f1b0ae0855
anonymoussamachar-prog <anonymoussamachar@gmail.com>
2026-05-04 12:33:12 +0530

    first commit

---
+++ src/main/java/com/cfs/Weather_App/dto/WeatherResponse.java
@@ -0,0 +1,62 @@
+package com.cfs.Weather_App.dto;
+
+public class WeatherResponse {
+
+
+    private String city;
+    private String condition;
+    private double temperature;
+    private String region;
+    private String country;
+
+    public WeatherResponse() {
+    }
+
+    public WeatherResponse(String city, String condition, double temperature, String region, String country) {
+        this.city = city;
+        this.condition = condition;
+        this.temperature = temperature;
+        this.region = region;
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
